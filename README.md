631e0b2f1c2a7ba5a03c6e0d90d13f51c8a0f338
Théo Laperrouse <theo.laperrouse@laposte.net>
2021-09-14 07:45:48 +0200

    Add gspread library

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
-# Crypto :
+# CryptoDaily :
 
 ## Description
 
-Script permettant de mettre à jour une fiche Google Sheet avec toutes les valeurs de nos Cryptos sur Binance 
+Script permettant de mettre à jour une fiche Google Sheet avec toutes les valeurs des Cryptos sur un compte Binance.
 
 ## Technologies :
 
 Cette application est faite grâce à Python3 et différentes APIs :
-- API de Google pour l'accès à Google Sheet
-- API de Binance pour la récupération du prix des cryptos et du nombre de Cryptos
+- API de Google pour l'accès à Google Sheet.
+- API de Binance pour la récupération du prix des cryptos et du nombre de Cryptos.
 
 ## QuickStart
 
-Définir les variables d'environnement binance_api et binance_key.
+Créer une Clé API depuis Binance qui permet d'accéder en lecture à vos Cryptomonnaie.
+
+Définir les variables d'environnement binance_api et binance_key sur votre ordinateur.
 
 Installation des librairies requises :
 ```pip3 install -r requirements.txt```
@@ -22,5 +24,5 @@ Lancement du Script :
 
 ## Autres Informations :
 
-Script envVariable.sh -> Définir une variable d'environnement sur Linux indéfiniment
+Script envVariable.sh -> Définir une variable d'environnement sur Linux indéfiniment.
 
--- scriptCrypto.py
+++ scriptCrypto.py
@@ -1,8 +1,10 @@
 import os
 from binance.client import Client
+import gspread
 
 api_key = os.environ.get('binance_api')
 api_secret = os.environ.get('binance_secret')
+
 
 
 def get_price(coin_symbol, coins_info):
@@ -11,6 +13,9 @@ def get_price(coin_symbol, coins_info):
                     None))
     return price
 
+def update_crypto(symbol, price):
+    return
+
 def main():
     client = Client(api_key, api_secret)
     assets = client.get_account()['balances']
@@ -18,8 +23,13 @@ def main():
     my_assets = [{'symbol' : asset['asset'],'number' : float(asset['free'])+float(asset['locked']), 'price': get_price(asset['asset'], coins_info) } 
                     for asset in assets if float(asset['free'])+float(asset['locked']) > 0]
     print(my_assets)
-    total_price = sum([asset['number']*asset['price']['value'] for asset in my_assets if asset['price'] != None])
+    total_price_eur = sum([asset['number']*asset['price']['value'] for asset in my_assets if asset['price'] != None and asset['price']['coin'] == 'EUR' ])
+    total_price_usdt = sum([asset['number']*asset['price']['value'] for asset in my_assets if asset['price'] != None and asset['price']['coin'] == 'USDT']) * 0.85
+    total_price = total_price_eur + total_price_usdt
     print(total_price)
+    
+    gc = gspread.service_account()
+    wks = gc.open("Cryptomonnaie").sheet1
 
 if __name__ == "__main__":
     main()
